4f377f6510b6489a87157dedd2f1b5d72cf4b518
osman <osmanalidev7@gmail.com>
2023-06-01 18:02:31 +0300

    work

--- service/initializeAllClients.js
+++ service/initializeAllClients.js
@@ -80,36 +80,35 @@ function initializeAllClients(app){
                       
                     if(Array.isArray(req.body)){
             
-                      for (let i = 0; i < req.body.length; i++) {
-            
-                        let nestedArray = req.body[i];
-            
-                        let phoneNumber = nestedArray['chatID'].toString().replace(/\+/g, "") + "@c.us";
-        
-                        if(nestedArray['type'] == 'text'){
-                        
-                            const text = nestedArray['text'] ;
+                        for (let i = 0; i < req.body.length; i++) {
+              
+                          let nestedArray = req.body[i];
+              
+                          let phoneNumber = nestedArray.chatID.toString().replace(/\+/g, "") + "@c.us";
+              
+                          if(nestedArray.type == 'text'){
+                          
+                            const text = nestedArray.text ;
                 
                             data = { chatId: phoneNumber, text: text };
-                
+                            console.log(data)
                             whatsappMassageQueue.add("massage", data , { delay: i * 10000 });
-            
-                        }
-        
-                        if(nestedArray['type'] == 'media' ){
-            
-                            const media = nestedArray['media'] ;
+              
+                          }
+              
+                          if(nestedArray.type == 'media' ){
+              
+                            const media = nestedArray.media ;
                 
                             data = { chatId: phoneNumber, media: media };
+              
+                            console.log(data)
                 
                             whatsappMassageQueue.add("massage", data , { delay: i * 10000 });
-            
+              
+                          } 
                         }
-                          
-            
-                        
-                      }
-            
+              
                     }
               
                     res.send("massage sended !");
